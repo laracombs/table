9c46b5b1a63a079b4978fdaf6251e0b6e4756d04
Norman Huth <norman@huth.it>
2024-08-21 21:12:51 +0200

    feat: add table events

---
+++ src/Events/TableSerialize.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace LaraCombs\Table\Events;
+
+/**
+ * @method static mixed dispatch(\LaraCombs\Table\AbstractTable $table)
+ */
+class TableSerialize extends TableCreated
+{
+    //
+}
